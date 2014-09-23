4c05140115f446b3b0dd7a2b7ef1ace1ba24317f
John Vieira <john.c.vieira@gmail.com>
2014-09-23 00:22:12 -0400

    new design layout

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,7 @@ var app = angular.module('mapPostersApp', ['ngRoute'])
 .config(['$routeProvider', function ($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'views/main.html'
+      templateUrl: 'views/homepage.html'
     })
     .otherwise({
       redirectTo: '/'
